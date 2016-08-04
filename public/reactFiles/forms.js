a61eafdf699c1405a3d83a319e49f293ee20b62c
Francisco Lozano <newreality64@gmail.com>
2016-08-03 23:20:23 -0600

    se agregó null como className
    No se necesita la clase para esto que estoy haciendo pero debe incluir el null la función de createElement

--- public/reactFiles/forms.js
+++ public/reactFiles/forms.js
@@ -1,9 +1,7 @@
 var Titulo = React.createClass({
   render: function() {
     return (
-      React.createElement('h1', 
-        this.props.t
-      )
+      React.createElement('h1', null, this.props.t)
     );
   }
 });
